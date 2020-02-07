4ec08cc5dcd2880b835ce3a881e7246943df8bc6
Jedrzej Szczepaniak <jedrzejszczepaniak@Jedrzejs-MacBook-Air.local>
2020-02-07 21:31:42 +0100

    Get go verions from modules

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/jbszczepaniak/whichgo
+
+go 1.13
+
+require (
+	github.com/gomods/athens v0.7.2
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/spf13/afero v1.2.2
+	golang.org/x/crypto v0.0.0-20200206161412-a0c6ece9d31a // indirect
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5 // indirect
+	gopkg.in/src-d/go-git.v4 v4.13.1
+)
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(dir)
+
+	cmd := exec.Command("git", "clone", "https://github.com/ThreeDotsLabs/watermill", dir)
+	cmd.Dir = dir
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	cmd = exec.Command("git", "ls-files", )
+	cmd.Dir = dir
+	out, err := cmd.Output()
+	br := bufio.NewReader(bytes.NewReader(out))
+	for {
+		read, err := br.ReadBytes('\n')
+		if err != nil{
+			if err == io.EOF {
+				break
+			}
+		}
+		trimmed := strings.TrimSuffix(string(read), "\n")
+		if strings.HasSuffix(trimmed, "go.mod") {
+			fmt.Println(goVerFromMod(filepath.Join(dir, trimmed)))
+		}
+	}
+}
+
+func goVerFromMod(file string) string {
+	f, err := os.Open(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "go") {
+			return line
+		}
+	}
+	return "NOT FOUND"
+}
